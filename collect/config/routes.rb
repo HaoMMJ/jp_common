46ff814bdabd6ba7ad03e28cb6b7db3ca34be50f
hao <hao@mmj.vn>
2016-09-28 00:15:12 +0700

    create file for check mazii jlpt kana

--- collect/config/routes.rb
+++ collect/config/routes.rb
@@ -71,6 +71,7 @@ Rails.application.routes.draw do
   get 'update_vocabulary_kanji' => 'vocabulary#update_vocabulary_kanji'
   get 'insert_missing_jlpt_kanji' => 'vocabulary#insert_missing_jlpt_kanji'
   get 'insert_jlpt_kana_without_kanji' => 'vocabulary#insert_jlpt_kana_without_kanji'
+  get 'get_jlpt_kana_from_mazii' => 'vocabulary#get_jlpt_kana_from_mazii'
 
   #kanji dictionary
   get 'filter_yoyo_kanji' => 'kanji_dictionary#filter_yoyo_kanji'
